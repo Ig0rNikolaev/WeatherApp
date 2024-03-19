7b858b61102ef2c316852672b0d27c0ca92c02e9
Ig0rNikolaev <igor01131987@gmail.com>
2024-03-19 17:22:51 +0300

    Added start settings.
    - added pods
    - added start setting

---
+++ WeatherApp/Sources/Scenes/WeatherScene/WeatherViewController.swift
@@ -0,0 +1,40 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Игорь Николаев on 19.03.2024.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+final class WeatherViewController: UIViewController {
+
+    //: MARK: - UI Elements
+
+    //: MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+        view.backgroundColor = .blue
+    }
+
+    //: MARK: - Actions
+
+    //: MARK: - Setups
+
+    private func setupView() {
+    }
+
+    private func setupHierarchy() {
+    }
+
+    private func setupLayout() {
+    }
+}
+
