001f4922b8ec3a6d54da18db95363dfde2e51ee3
Ig0rNikolaev <igor01131987@gmail.com>
2024-03-19 16:45:55 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Игорь Николаев on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
